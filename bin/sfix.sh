eea172535a4f89fed8e354c75297d020b494d682
Ryan Moore <ryan.moore@grandrounds.com>
2016-01-19 17:24:14 -0800

    1920x1080 mirror mode from cmdline

---
+++ bin/sfix.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+#xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2-1 --mode 2560x1440 --pos 0x0 --rotate normal
+xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2-1 --mode 1920x1080 --pos 0x0 --rotate normal
