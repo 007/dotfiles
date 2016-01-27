0b1736d29b5ab0fe8917d7f43e049b7688764916
Ryan Moore <ryan.moore@grandrounds.com>
2016-01-27 15:49:52 -0800

    better screen fix - auto-pick highest of 2560 or 1920 resolutions

--- bin/sfix.sh
+++ bin/sfix.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-/usr/bin/xrandr --output eDP1 --mode 1920x1080 --pos 0x1440 --rotate normal --output DP2-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+
+EXTERNAL_MONITOR=$(xrandr | grep -Eo '^.{3,5} connected ' | cut -d\  -f1 | grep -v '^eDP1$' | head -1)
+BEST_RESOLUTION=$(xrandr | grep -A4 "^${EXTERNAL_MONITOR} " | grep -v connected | awk '{print $1}' | grep -E '(2560x1440|1920x1080)' | head -1)
+
+/usr/bin/xrandr --output eDP1 --auto --output ${EXTERNAL_MONITOR} --primary --mode ${BEST_RESOLUTION} --above eDP1
