1ad32baacf24c150ab6075749442dbb0f073dee9
Ryan Moore <ryan.moore@grandrounds.com>
2016-02-15 13:00:49 -0800

    support 16x10 in addition to 16x9 aspect

--- bin/sfix.sh
+++ bin/sfix.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 EXTERNAL_MONITOR=$(xrandr | grep -Eo '^.{3,5} connected ' | cut -d\  -f1 | grep -v '^eDP1$' | head -1)
-BEST_RESOLUTION=$(xrandr | grep -A4 "^${EXTERNAL_MONITOR} " | grep -v connected | awk '{print $1}' | grep -E '(2560x1440|1920x1080)' | head -1)
+BEST_RESOLUTION=$(xrandr | grep -A4 "^${EXTERNAL_MONITOR} " | grep -v connected | awk '{print $1}' | grep -E '(2560x1440|2560x1600|1920x1080|1920x1200)' | head -1)
 
 /usr/bin/xrandr --output eDP1 --auto --output ${EXTERNAL_MONITOR} --primary --mode ${BEST_RESOLUTION} --above eDP1
