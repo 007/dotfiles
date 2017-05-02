4b31ea212fcbd585e87ac95a0f92b30ce896f314
Ryan Moore <ryan.moore@grandrounds.com>
2017-05-02 12:29:37 -0700

    exclude anaconda envs

--- bin/backup_home.sh
+++ bin/backup_home.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo mount /mnt/kingston
 DATESTAMP=$(date +%Y%m%d)
-sudo rsync -aP --del --exclude .rvm/ --exclude .bundle/ --exclude .cache/ --exclude Videos/ --exclude old.sqsh --exclude '*.iso' /home/rmoore/ /mnt/kingston/rmoore/
+sudo rsync -aP --del --exclude .rvm/ --exclude .bundle/ --exclude .cache/ --exclude Videos/ --exclude anaconda3/ --exclude old.sqsh --exclude '*.iso' /home/rmoore/ /mnt/kingston/rmoore/
 sudo btrfs subvolume sync /mnt/kingston/rmoore/
+echo "waiting for a bit so you can review"
+sleep 180
 sudo btrfs subvolume snapshot -r /mnt/kingston/rmoore/ /mnt/kingston/backup/${DATESTAMP}
 sudo btrfs subvolume sync /mnt/kingston/backup/${DATESTAMP}
 sudo btrfs filesystem sync /mnt/kingston/
