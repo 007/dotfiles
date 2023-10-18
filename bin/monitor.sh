5161857262c930c5a2eb3efa08f64c1724c4242e
Ryan Moore <rmoore@aurora.tech>
2023-10-18 18:27:02 +0000

    smaller monitor setup

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -2,7 +2,7 @@
 
 read -s -p "Sudo Password: " password
 
-tmux new-session -d -A -s monitor htop -u rmoore -s PERCENT_CPU
+tmux new-session -d -A -s monitor htop -d 5 -u rmoore -s PERCENT_CPU
 tmux split-window -v watch -n 10 "free -h;df -h --output / /home /tmp /ephemeral"
 
 # new sudo window, then switch to activate it and sendkeys password^M
@@ -16,13 +16,13 @@ tmux split-window -v watch -n 10 nvidia-smi
 tmux select-pane -t 3
 tmux resize-pane -U 999
 
-# resize htop to fit 64 CPUs and 8 processes
+# resize htop to fit 16 CPUs and 2 processes
 tmux select-pane -t 0
-tmux resize-pane -D 31
+tmux resize-pane -D 12
 
 # Resize free/df to fit 4 filesystems
 tmux select-pane -t 1
-tmux resize-pane -D 15
+tmux resize-pane -D 10
 
 # Resize iotop down to fit just the important bits of nvidia-smi
 tmux select-pane -t 2
@@ -30,4 +30,3 @@ tmux resize-pane -D 10
 
 # now attach, so we can run `ssh -t vdesk bin/monitor.sh` all-in-one
 tmux -2 attach-session -t monitor
-
